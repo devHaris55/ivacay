e5e49db57e636892d638602bdf72d06c02cd49f5
devHaris55 <haris.dev55@gmail.com>
2022-03-31 03:43:57 +0500

    sequence the script tags in js.blade.php

--- resources/views/guider/layouts/js.blade.php
+++ resources/views/guider/layouts/js.blade.php
@@ -1,9 +1,9 @@
 <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
 <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js" ></script> -->
-<script src="{{asset('guider/js/all.js')}}"></script>
-<script src="{{asset('js/bootstrap.js')}}"></script>
-
 <script src="{{asset('js/jquery-3.6.0.min.js')}}"></script>
+<script src="{{asset('guider/js/all.js')}}"></script>
+
+<script src="{{asset('js/bootstrap.js')}}"></script>
 <script src="{{asset('guider/js/custom.js')}}"></script>
 
 <!-- For stripe -->
--- resources/views/layouts/js.blade.php
+++ resources/views/layouts/js.blade.php
@@ -1,7 +1,6 @@
-<script src="{{asset('js/aos.js')}}"></script>
-<script src="{{asset('js/bootstrap.js')}}"></script>
+
 <!-- <script src="{{asset('js/core.js')}}"></script> -->
-<script src="{{asset('js/custom.js')}}"></script>
+
 <script src="{{asset('js/jquery-3.6.0.min.js')}}"></script>
 <script src="{{asset('js/jquery.fancybox.min.js')}}"></script>
 <script src="{{asset('js/jquery.slicknav.js')}}"></script>
@@ -10,50 +9,11 @@
 <script src="{{asset('js/wow.js')}}"></script>
 <script src="{{asset('slick/slick.js')}}"></script>
 <script src="{{asset('slick/slick.min.js')}}"></script>
-
+<script src="{{asset('js/aos.js')}}"></script>
+<script src="{{asset('js/bootstrap.js')}}"></script>
+<script src="{{asset('js/custom.js')}}"></script>
 
 <!-- For stripe -->
 <script type="text/javascript" src="https://js.stripe.com/v2/"></script>
-
-
-<script>
-  
-
-  // blogslider start
-  $(".blogslid").slick({
-      dots: false,
-      arrows: true,
-      infinite: true,
-      speed: 300,
-      autoplay: true,
-      slidesToShow: 3,
-      slidesToScroll: 1,
-      responsive: [
-          {
-              breakpoint: 1024,
-              settings: {
-                  slidesToShow: 3,
-                  slidesToScroll: 1,
-                  infinite: true,
-                  dots: true,
-              },
-          },
-          {
-              breakpoint: 600,
-              settings: {
-                  slidesToShow: 2,
-                  slidesToScroll: 2,
-              },
-          },
-          {
-              breakpoint: 480,
-              settings: {
-                  slidesToShow: 1,
-                  slidesToScroll: 1,
-              },
-          },
-      ],
-  });
-</script>
 
 @stack('js')
