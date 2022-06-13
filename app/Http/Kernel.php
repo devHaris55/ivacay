bcfc1302e754e1828e5b8623cbf93070a5cdeb45
devHaris55 <haris.dev55@gmail.com>
2022-06-13 23:55:25 +0500

    sanctum integration

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -40,14 +40,14 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
-        'protectedPage'=>[
+        'protectedPage' => [
             \App\Http\Middleware\SessionMiddleware::class,
         ],
-        'guiderRoleMiddleware'=>[
+        'guiderRoleMiddleware' => [
             \App\Http\Middleware\RoleMiddleware::class,
         ],
     ];
